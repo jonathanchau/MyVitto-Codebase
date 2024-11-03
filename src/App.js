cc997113203201c049404568fe8645e5f904ca0e
andrewpho942 <122491079+andrewpho942@users.noreply.github.com>
2024-11-02 22:30:47 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,4 +21,42 @@ function App() {
     </div>
   );
 }
+
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { AuthProvider } from "../../services/AuthContext.js"
+// import PrivateRoute from "../../services/PrivateRoute.js";
+// import HomePage from "../../containers/home-page/HomePage.js";
+// import LoginPage from "../../containers/login-page/LoginPage.js";
+// import RegistrationPage from "../../containers/registration-page/RegistrationPage.js";
+// import Dashboard from "../../containers/dashboard-page/Dashboard.js";
+// import ForgotPassword from "../../containers/forgot-password-page/ForgotPassword.js";
+// import ContactUs from "../../containers/contact-us-page/ContactUs.js";
+// import ResetPassword from "../../containers/reset-password-page/ResetPassword.js";
+// import SettingsPage from "../../containers/settings-page/SettingsPage.js";
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <AuthProvider>
+//         <BrowserRouter>
+//           <Routes>
+//             <Route exact path="/" element={<HomePage />} />
+//             <Route path="Contact-Us" element={<ContactUs />} />
+//             <Route path="Login" element={<LoginPage />} />
+//             <Route path="Sign-Up" element={<RegistrationPage />} />
+//             <Route path="forgot-password" element={<ForgotPassword />} />
+//             <Route path="reset-password" element={<ResetPassword />} />
+//             <Route path="Dashboard/:user" element={<PrivateRoute />}>
+//               <Route exact path="" element={<Dashboard />} />
+//               <Route exact path="settings" element={<SettingsPage />} />
+//             </Route>
+//             <Route path="*" element={NotFound} />
+//           </Routes>
+//         </BrowserRouter>
+//       </AuthProvider>
+//     </div>
+//   );
+// }
+
 export default App;
