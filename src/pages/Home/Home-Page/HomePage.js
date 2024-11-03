01a7327afc3ba6b4630342763260ba881153fa8a
andrewpho942 <122491079+andrewpho942@users.noreply.github.com>
2024-11-02 21:53:14 -0700

    Add files via upload

---
+++ src/pages/Home/Home-Page/HomePage.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './HomePage.css';
+import Navbar from "../../components/nav-bar/Navbar.js";
+import Text from "../../images/Text.png"
+import JellyFishImagePage1 from "../../images/JellyFishImagePage1.png"
+import Vittotext2 from "../../images/Vittotext2.png"
+import VittoSplashBackground2 from "../../images/VittoSplashBackground2.png"
+import SoHowDoesVittoWork from "../../images/SoHowDoesVittoWork.png"
+import Bubble1 from "../../images/Bubble1.png"
+import Bubble2 from "../../images/Bubble2.png"
+import Bubble3 from "../../images/Bubble3.png"
+import Bubble4 from "../../images/Bubble4.png"
+
+// import img1 from './images/JellyFishImagePage1.png'
+
+
+function HomePage() {
+  return (
+    <div>
+      <Navbar />
+      {/* Top Part */}
+      <div className="section1">
+        <div className="gridContainer">
+          <div className="topLeftText">
+            <img src={Text} alt="" className="textStyle" /> {/* Networking Simplified with Vitto */}
+            <button className="buttonStyle">Get Started</button>
+          </div>
+          <div className="topJellyFish">
+            <img src={JellyFishImagePage1} alt="" className="image1Style" />
+          </div>
+          <div className="bottomRightText">
+            <img src={Vittotext2} alt="" className="textStyle2" />
+          </div>
+        </div>
+        {/* <div className="getStartedButton">
+        </div> */}
+        <div className="splashImage">
+          <img src={VittoSplashBackground2} alt="" className="image2Style" />
+        </div>
+      </div>
+      
+      <div className="section1-transition"></div>
+
+      {/* Middle Part */}
+      <div className="section2">
+        <img src={SoHowDoesVittoWork} alt="" className="textStyleSection2" />
+
+        <div className="bubbleContainer">
+          <img src={Bubble1} alt="" className="bubbleStyle" />
+          <img src={Bubble2} alt="" className="bubbleStyle" />
+          <img src={Bubble3} alt="" className="bubbleStyle" />
+          <img src={Bubble4} alt="" className="bubbleStyle" />
+        </div>
+      </div>
+
+      <div className="section2-transition"></div>
+
+      {/* Bottom Part */}
+      <div className="section3">
+      
+      </div>
+
+    </div>
+  );
+}
+
+export default HomePage;
