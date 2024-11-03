c030712ada22f5789cb2d163421bfe845f1fb1fc
Howard <120993394+howardleesays@users.noreply.github.com>
2024-11-02 21:57:32 -0700

    Add files via upload

---
+++ src/pages/Card-Customization/Qr-Code/Input-Area/InputArea.js
@@ -0,0 +1,12 @@
+import './InputArea.css';
+
+const InputArea = () => {
+    return (  
+        <div className="input-container">
+            <div className="icon"/>
+            <textarea className="input"/>
+        </div>
+    );
+}
+ 
+export default InputArea;
