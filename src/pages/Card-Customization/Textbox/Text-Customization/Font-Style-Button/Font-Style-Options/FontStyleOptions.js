aadd73ea0a45afa9f1ab9c97db7f0fe5eb95ad72
andrewpho942 <122491079+andrewpho942@users.noreply.github.com>
2024-11-02 22:18:09 -0700

    Add files via upload

---
+++ src/pages/Card-Customization/Textbox/Text-Customization/Font-Style-Button/Font-Style-Options/FontStyleOptions.js
@@ -0,0 +1,72 @@
+import OneFontStyleOption from "./OneFontStyleOption/OneFontStyleOption";
+import './FontStyleOptions.css';
+import {useState, useEffect} from "react";
+
+const FontStyleOptions = ({textboxRef, setStyleOptionsAreDisplayed, setFontStyleName, isHoveringOverFontStyleButton}) => {
+
+    const [isHoveringOverStyleOptions, setIsHoveringOverStyleOptions] = useState(null);
+
+    const handleMouseEnterStyleOptions = () => {
+        setIsHoveringOverStyleOptions(true);
+    }
+
+    const handleMouseLeaveStyleOptions = () => {
+        setIsHoveringOverStyleOptions(false);
+    }
+
+    useEffect(() => {
+        const handleClick = () => {
+            if (!isHoveringOverStyleOptions && !isHoveringOverFontStyleButton) {
+                setStyleOptionsAreDisplayed(false);
+            }
+        }
+
+        window.addEventListener("click", handleClick);
+
+        return () => {
+            window.removeEventListener("click", handleClick);
+        }
+
+    }, [isHoveringOverStyleOptions, isHoveringOverFontStyleButton])
+    return ( 
+        <div 
+            className="font-style-options-container"
+            onMouseEnter={handleMouseEnterStyleOptions}
+            onMouseLeave={handleMouseLeaveStyleOptions}
+        >
+            <OneFontStyleOption 
+                textboxRef={textboxRef}
+                fontStyle={'Times New Roman'}
+                top={'0px'}
+                bottomBorderColor={'1px solid #ddd'}
+                setStyleOptionsAreDisplayed={setStyleOptionsAreDisplayed}
+                setFontStyleName={setFontStyleName}
+            />
+            <OneFontStyleOption 
+                textboxRef={textboxRef}
+                fontStyle={'Arial'}
+                top={'30px'}
+                bottomBorderColor={'1px solid #ddd'}
+                setStyleOptionsAreDisplayed={setStyleOptionsAreDisplayed}
+                setFontStyleName={setFontStyleName}
+            />
+            <OneFontStyleOption 
+                textboxRef={textboxRef}
+                fontStyle={'Georgia'}
+                top={'60px'}
+                bottomBorderColor={'1px solid #ddd'}
+                setStyleOptionsAreDisplayed={setStyleOptionsAreDisplayed}
+                setFontStyleName={setFontStyleName}
+            />
+            <OneFontStyleOption 
+                textboxRef={textboxRef}
+                fontStyle={'Lora'}
+                top={'90px'}
+                setStyleOptionsAreDisplayed={setStyleOptionsAreDisplayed}
+                setFontStyleName={setFontStyleName}
+            />
+        </div>
+    );
+}
+ 
+export default FontStyleOptions;
